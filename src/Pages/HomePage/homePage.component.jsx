1d1c46bb41b83533b895d5b53c8284eea85be1b7
walidtouj <walidtouj@gmail.com>
2021-11-08 23:00:45 +0100

    create homepage

---
+++ src/Pages/HomePage/homePage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./homePage.styles.scss"
+import Directory from '../../components/Directory/Directory.component';
+const HomePage= ()=>(
+    <div className="homepage">
+    <Directory/>
+    </div>
+
+)
+
+export default HomePage;
---
+++ src/components/Menu-Item/Menu-Item.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Menu-Item.styles.scss"
+
+const MenuItem = ({title,imageUrl,size})=>(
+    <div className={`${size} menu-item`} style={{backgroundImage:`url(${imageUrl})`}}>
+    <div className="content">
+        <h1 className="title">{title}</h1>
+        <sapn className="subtitle">SHOP NOW</sapn>
+    </div>
+</div>
+)
+
+export default MenuItem;
