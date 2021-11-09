a2e7e7577142fed86a92b2f2ed93da8b004a1bc3
walidtouj <walidtouj@gmail.com>
2021-11-09 23:12:47 +0100

    update our menu-item

--- src/components/Menu-Item/Menu-Item.component.jsx
+++ src/components/Menu-Item/Menu-Item.component.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import "./Menu-Item.styles.scss"
 
 const MenuItem = ({title,imageUrl,size})=>(
-    <div className={`${size} menu-item`} style={{backgroundImage:`url(${imageUrl})`}}>
+    <div className={`${size} menu-item`} >
+        <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}/>
     <div className="content">
         <h1 className="title">{title}</h1>
         <sapn className="subtitle">SHOP NOW</sapn>
